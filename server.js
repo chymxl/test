d3873eed314d3a57088cbb90afb285b4cf8db033
chymxl <904127184@qq.com>
2017-03-12 17:24:50 +0800

    hot reload

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017/3/12.
+ */
+var express = require("express");
+var webpack = require("webpack");
+var path = require("path");
+
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpackHotMiddleware = require("webpack-hot-middleware");
+var webpackConfig = require("./webpack.config");
+
+var app = express();
+
+var compiler = webpack(webpackConfig);
+app.use(webpackDevMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
+app.use(express.static('public'));
+
+app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 /**
  * Created by Administrator on 2017/3/10.
  */
+var webpack = require("webpack");
+var path = require("path");
+
 module.exports = {
     devtool: 'eval-source-map', //生成source-map
-    entry : __dirname + '/app/main.js',
+    entry : [
+        'webpack-hot-middleware/client?path=http:127.0.0.1:3000/__webpack_hmr&reload=true&noInfo=false&quiet=false',
+        './app/main.js'
+    ],
     output: {
         path : __dirname + '/public',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: "/public/",
+        chunkFilename: 'bundle.js'
     },
     module: {
         loaders: [
@@ -25,6 +33,11 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
     devServer: {
         contentBase: "./public",//本地服务器加载页面所在目录
         colors: true, //终端中输出结果为彩色
